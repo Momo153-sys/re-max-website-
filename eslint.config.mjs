4db6d9a3b764148f84fe9653f6aee98f2a0766ca
Momo153-sys <154274664+Momo153-sys@users.noreply.github.com>
2025-04-24 03:22:19 +0300

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,7 @@ const eslintConfig = [
       '@typescript-eslint/no-require-imports': 'off',
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   }),
 ];
